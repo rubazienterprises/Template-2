899e190d92a9b34eee9b530e244ccf90eb1dd6c8
Abdul Aziz <rubazienterprises@gmail.com>
2022-06-19 18:44:28 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function ShowHideMenu() {
+  var x = document.getElementById("automenu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
